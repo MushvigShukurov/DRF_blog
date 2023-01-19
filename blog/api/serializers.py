e19dd9db122a36d4d1698a48f1d5a3d3cfdb1f99
MushvigShukurov <musfiqsukurov9@gmail.com>
2023-01-19 22:16:48 +0400

    Model Serializer

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,47 +1,67 @@
 from rest_framework import serializers
 from blog.models import Blog
-class BlogSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    author = serializers.CharField()
+from datetime import datetime
+from django.utils.timesince import timesince
+from django.utils import timezone
+# Model Serializer
+class BlogSerializer(serializers.ModelSerializer):
+    gun_evvel = serializers.SerializerMethodField()
+    class Meta:
+        model = Blog 
+        fields = "__all__"
+        # fields = ['title']
+        # exclude = ['title']
+        # read_only_fields = ['id','title']
+    def get_gun_evvel(self,object):
+        now = timezone.now()
+        cr_at = object.created_at
+        gunler = timesince(cr_at,now)
+        return gunler + " ago"
 
-    title = serializers.CharField()
-    description = serializers.CharField()
-    content = serializers.CharField()
-    slug = serializers.SlugField(read_only=True)
-    is_delete = serializers.BooleanField()
-
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
 
 
+# class BlogSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     author = serializers.CharField()
 
-    def create(self, validated_data):
-        return Blog.objects.create(**validated_data)
+#     title = serializers.CharField()
+#     description = serializers.CharField()
+#     content = serializers.CharField()
+#     slug = serializers.SlugField(read_only=True)
+#     is_delete = serializers.BooleanField()
+
+#     created_at = serializers.DateTimeField(read_only=True)
+#     updated_at = serializers.DateTimeField(read_only=True)
 
 
-    def update(self, instance, validated_data):
-        instance.author = validated_data.get('author',instance.author)
-        instance.title = validated_data.get('title',instance.title)
-        instance.description = validated_data.get('description',instance.description)
-        instance.content = validated_data.get('content',instance.content)
-        instance.slug = validated_data.get('slug',instance.slug)
-        instance.is_delete = validated_data.get('is_delete',instance.is_delete)
-        instance.save()
-        return instance
 
-    def validate(self,data): # object Level
-        if len(data['title']) <10:
-            raise serializers.ValidationError(
-                "Title minimum 10 xarakterden ibaret olmalidir."
-            )
-        return data
+#     def create(self, validated_data):
+#         return Blog.objects.create(**validated_data)
 
-    # def validate_slug(self, value):
-    #     has_slug = Blog.objects.filter(slug=value).count()
-    #     print(value)
-    #     print("HAS SLUG :",has_slug)
-    #     if has_slug > 0:
-    #         raise serializers.ValidationError(
-    #             "Slug fields are unique!"
-    #         )
-    #     return value
+
+#     def update(self, instance, validated_data):
+#         instance.author = validated_data.get('author',instance.author)
+#         instance.title = validated_data.get('title',instance.title)
+#         instance.description = validated_data.get('description',instance.description)
+#         instance.content = validated_data.get('content',instance.content)
+#         instance.slug = validated_data.get('slug',instance.slug)
+#         instance.is_delete = validated_data.get('is_delete',instance.is_delete)
+#         instance.save()
+#         return instance
+
+#     def validate(self,data): # object Level
+#         if len(data['title']) <10:
+#             raise serializers.ValidationError(
+#                 "Title minimum 10 xarakterden ibaret olmalidir."
+#             )
+#         return data
+
+#     # def validate_slug(self, value):
+#     #     has_slug = Blog.objects.filter(slug=value).count()
+#     #     print(value)
+#     #     print("HAS SLUG :",has_slug)
+#     #     if has_slug > 0:
+#     #         raise serializers.ValidationError(
+#     #             "Slug fields are unique!"
+#     #         )
+#     #     return value
