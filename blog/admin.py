90b2a98d50478ab648819b2999c882bf11c1b12e
MushvigShukurov <musfiqsukurov9@gmail.com>
2023-01-20 09:46:27 +0400

    BlogRaiting + M2M Relation

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from blog.models import Blog
+from blog.models import Blog, Comment, BlogRaiting
 # Register your models here.
 # admin.site.register(Blog)
-
+admin.site.register(BlogRaiting)
+admin.site.register(Comment)
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     readonly_fields = ['slug']
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from slugify import slugify
 from ckeditor.fields import RichTextField
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Blog(models.Model):
     # author = models.CharField(max_length=50,null=True,blank=True)
@@ -12,6 +13,10 @@ class Blog(models.Model):
     content = RichTextField()
     slug = models.SlugField(max_length=150,null=True,blank=True)
     is_delete = models.BooleanField(default=False)
+    
+    # raiting = models.IntegerField(validators=[MinValueValidator,MaxValueValidator],default=0)
+    # many to many olmalidir reytinq bir user bir cox kitaba ulduz vere biler
+    # bir kitaba birden cox user ulduz vere biler
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -28,6 +33,42 @@ class Blog(models.Model):
         return self.title
 
     
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog,on_delete=models.CASCADE,related_name="şərhlər")
+    comment_author_firstname = models.CharField(max_length=50)
+    comment_author_lastname = models.CharField(max_length=50)
+    comment_author_email = models.CharField(max_length=100)
+    comment_text = models.TextField(blank=True,null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 
+class BlogRaiting(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="reytinqler")
+    blog = models.ForeignKey(Blog,on_delete=models.CASCADE, related_name="reytinqler")
+    raiting = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)],default=5)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
+    def save(self, *args, **kvargs):
+        raitings_for_user = BlogRaiting.objects.filter(user=self.user,blog=self.blog).count()
+        if raitings_for_user == 0:
+            super(BlogRaiting,self).save(*args,**kvargs)
+        elif raitings_for_user == 1:
+            up_raiting = BlogRaiting.objects.filter(user=self.user,blog=self.blog).first()
+            self.created_at = up_raiting.created_at
+            up_raiting.delete()
+            super(BlogRaiting,self).save(*args,**kvargs)
+
+
+
+
+
+    def __str__(self) -> str:
+        return "BLOG: " + self.blog.title + " | Reytinq: " + str(self.raiting) + " | " + "USER :" + self.user.username
